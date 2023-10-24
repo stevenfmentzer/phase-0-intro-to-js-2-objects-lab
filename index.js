d62a31d96d04824c34ea83c09b9caacf070f6460
Steven Mentzer <stevenfmentzer@gmail.com>
2023-10-24 16:54:41 -0400

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+let employee = {};
+employee.name();
+employee.streetSddress();
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+ }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const updateEmployee = {...employee};
+    delete updateEmployee[key];
+    return updateEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
